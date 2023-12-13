514cb139ba3dae2b27f7f9884160108238e44b92
Ukinebo1 <perfectprecious2022@gmail.com>
2023-12-13 14:41:56 +0100

    group 9

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 
-const input = document.querySelector('#cityInput');
-const button = document.querySelector('#Btn');
+document.addEventListener('DOMContentLoaded', function () {
+  const input = document.querySelector('#cityInput');
+  const button = document.querySelector('#Btn');
  
-async function submitButton() {
-  const inputValue = input.value;
+  async function submitButton(event) {
+    event.preventDefault();
+ 
+    const inputValue = input.value;
+ 
+    const apiKey = "597c40c39084687093b091cd48b366f8";
+    const city = inputValue;
+ 
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`);
+      const data = await response.json();
+      console.log(data,"this is the data===============>")
  
  
-  const apiKey = "597c40c39084687093b091cd48b366f8";
-  const city = "Japen";
- 
- 
-  try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
-    const data = await response.json();
-    console.log(data,"api data==============>")
- 
-    const weatherDataContainer = document.getElementById('weatherData');
-    weatherDataContainer.innerHTML = `
-      <h2>${data.name}, ${data.sys.country}</h2>
-      <p>Temperature: ${data.main.temp}°C</p>
-      <p>Weather: ${data.weather[0].description}</p>
-    `;
-  } catch (error) {
-    console.error('Error fetching weather data:', error);
- 
+      const weatherDataContainer = document.getElementById('weatherData');
+      weatherDataContainer.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>${data.weather[0].icon}</p>
+      `;
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
   }
-}
  
-button.addEventListener('click', submitButton);
- 
+  button.addEventListener('click', submitButton);
+});
